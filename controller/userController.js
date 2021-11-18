dfe782ed04c18b1d91cd27af2be22eca1a48e5e7
Bhumit khatri <jaat@Bhumits-MacBook-Air.local>
2021-11-18 10:28:13 +0530

    JWT token

--- controller/userController.js
+++ controller/userController.js
@@ -3,38 +3,66 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 async function saveUserDetails(input) {
+    console.log("2122ws", input);
     try {
-        const newUser = new users({
-            name: input.name,
-            email: input.email,
-            password: input.password,
-            token: input.token
-        })
-            const oldUser = await users.findOne({ email: input.email });
-            if (oldUser) {
-      return "User Already Exist. Please Login";
-    }
-    encryptedPassword = await bcrypt.hash(password, 10);
+        const oldUser = await users.findOne({ email: input.email });
+        if (oldUser) {
+            return "User Already Exist. Please Login";
+        }
+        const token = jwt.sign(
+            { email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h",
+            }
+        );
+        // const encryptedPassword = await bcrypt.hash(input.password, 10);
+        // console.log("token", token, "##3s", encryptedPassword);
+        // const newUser = new users({
+        //     name: input.name,
+        //     email: input.email,
+        //     password: encryptedPassword,
+        //     token: token
+        // })
+        const encryptedPassword = await bcrypt.hash(password, 10);
 
-    // Create user in our database
-    const user = await users.create({
-      name,
-      email: email.toLowerCase(),
-      password: encryptedPassword,
-    });
-    const token = jwt.sign(
-      { user_id: user._id, email },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
-    users.token = token;
-    const UserDetails = await newUser.save()
-    return UserDetails;
+        // Create user in our database
+        const user = await users.create({
+            name: input.name,
+            email: input.email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword,
+        });
+        console.log("user", user);
+        console.log("env", process.env.TOKEN_KEY);
+        // Create token
+        const token = jwt.sign(
+            input.email,
+            "09876542312314438abdhchsihhfwiehuihiwgvvwshjnndkfiuhweiuh",
+            {
+                expiresIn: "2h",
+            }
+        );
+        console.log("token", token);
+        // save user token
+        user.token = token;
+
+        // const userDetails = await newUser.save()
+        console.log("user", user);
+
+
+        // Create user in our database
+        // const user = await users.create({
+        //   name,
+        //   email: email.toLowerCase(),
+        //   password: encryptedPassword,
+        // });
+
+        // users.token = token;
+        // const UserDetails = await newUser.save()
+        return user;
     } catch (err) {
         return err;
     }
 }
 
-module.exports= {saveUserDetails}
+module.exports = { saveUserDetails }
--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,14 @@ const routes = express.Router();
 const userController = require("../controller/userController");
 
 routes.post("/user/register", async (req, res) => {
-    const User = await userController.saveUserDetails(req.body);
     const { name, email, password } = req.body;
     if (!(email && password && name)) {
         res.status(400).send("All input is required");
-      }
-      else {
-    if (User) {
-        res.status(201).json(User)
+      } else {
+    console.log("*UU2ww", req.body);
+        const user = await userController.saveUserDetails(req.body);
+    if (user) {
+        res.status(201).json(user)
     } else {
         res.status(400).send("User not saved")
     }
